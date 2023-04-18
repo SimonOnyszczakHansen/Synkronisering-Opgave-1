2bd0f4fbb1d40b17da5a7b28bd8df1c93e54d920
Simon <Simon@DESKTOP-AFA0UEO>
2023-04-18 10:33:09 +0200

    Add project files.

---
+++ SynkroniseringØvelser/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynkroniseringØvelser
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static readonly object obj = new object();
+
+        public void CountUp()
+        {
+            while (true)
+            {
+                Monitor.Enter(obj);
+                try
+                {
+                    counter += 2;
+                }
+                finally
+                {
+                    Monitor.Exit(obj);
+                }
+                Console.WriteLine(counter);
+                Thread.Sleep(1000);
+            }
+        }
+        public void CountDown() 
+        {
+            while(true) 
+            {
+                Monitor.Enter(obj);
+                try
+                {
+                    counter--;
+                }
+                finally 
+                {
+                    Monitor.Exit(obj);
+                }
+                Console.WriteLine(counter);
+                Thread.Sleep(1000);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Program pg = new Program(); 
+            Thread t1 = new Thread(pg.CountUp);
+            Thread t2 = new Thread(pg.CountDown);
+            t1.Start();
+            t2.Start();
+            Console.Read();
+        }
+    }
+}
